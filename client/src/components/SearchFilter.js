9d1ddbb9a83cab34b40ea0b57a9d2f0c9ba013c7
J <09.chen@gmail.com>
2023-07-22 21:09:12 -0700

    Activity log updated

--- client/src/components/SearchFilter.js
+++ client/src/components/SearchFilter.js
@@ -4,7 +4,7 @@ const SearchFilter= (props) => {
     return (
       <>
   <div class="searchCard">
-  <div className="img">
+  <div className="imgIcon">
     <img src={props.icon} alt="" className="searchIcon"/>
   </div>
     <h4>{props.activity}</h4>
