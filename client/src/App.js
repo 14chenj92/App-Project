7a639a11520721fd4e893243d2173e27015e1a94
J <09.chen@gmail.com>
2023-08-03 14:35:31 -0700

    Added antdesign

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,10 @@ function App() {
                 element={<Signup />}
               />
               </Routes>
+          <body>
+          <div id="container" style="padding: 24px" />
+          <script>const mountNode = document.getElementById('container');</script>
+        </body>
     </div>
     </Router>
   );
---
+++ client/src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Demo from './components/Calendar';
+
+createRoot(document.getElementById('container')).render(<Demo />);
